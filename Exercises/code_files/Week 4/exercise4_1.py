8ccd9af9784d89910c08794ad5df3cb0c0fe931e
Laura <lredmondson1@sheffield.ac.uk>
2020-03-04 12:15:21 +0000

    Add code files for exercises week 4

---
+++ Exercises/code_files/Week 4/exercise4_1.py
@@ -0,0 +1,13 @@
+# Exercise 4.1: When I'm travelling in the winter I often forget to pack warm clothes.
+# Let's write a program to help me to remember the right clothes.
+# The program should check if the first item in the clothes list is "shorts" . If it is it
+# should change the value to "warm coat".
+
+# Extension: Change the other items in the list to clothing more appropriate to winter if the
+# first item is shorts.
+
+clothes = [
+    "shorts",
+    "shoes",
+    "t-shirt",
+]
---
+++ Exercises/code_files/Week 4/exercise4_6.py
@@ -0,0 +1,9 @@
+# Exercise 4.6: Using a for loop, output the values name , colour and price of each dictionary in the list.
+
+# Extension: Add more items to the list.
+
+fruits = [
+    {'name': 'apple', 'colour': 'red', 'price': 0.12},
+    {'name': 'banana', 'colour': 'yellow', 'price': 0.2},
+    {'name': 'pear', 'colour': 'green', 'price': 0.19},
+]
